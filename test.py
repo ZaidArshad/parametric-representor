fd1309eef66a6c1964a6e68658563248b8328d23
Arlo Watts <67204708+arlowatts@users.noreply.github.com>
2025-11-17 16:49:05 -0800

    Add meshing to visualization

--- test.py
+++ test.py
@@ -1,29 +1,41 @@
 import trimesh
 import numpy as np
 
-e1 = 1.7
-e2 = 1
+e1 = 2.3
+e2 = 1.2
 a = [1, 1, 1]
-points = []
+
+resolution = 32
+
+def main():
+
+    vertices = []
+    faces = []
+
+    for n in np.linspace(-np.pi / 2, np.pi / 2, resolution // 2):
+        for w in np.linspace(-np.pi, np.pi, resolution + 1):
+
+            x = a[0] * cos_pow(n, e1) * cos_pow(w, e2)
+            y = a[1] * cos_pow(n, e1) * sin_pow(w, e2)
+            z = a[2] * sin_pow(n, e1)
+
+            vertices.append([x, y, z])
+
+    for n in range(resolution // 2 - 1):
+        for w in range(resolution):
+
+            faces.append([w + n * (1 + resolution), (1 + w) + n * (1 + resolution), w + (1 + n) * (1 + resolution)])
+            faces.append([w + (1 + n) * (1 + resolution), (1 + w) + n * (1 + resolution), (1 + w) + (1 + n) * (1 + resolution)])
+
+    superquadric = trimesh.Trimesh(vertices, faces)
+
+    scene = trimesh.Scene([superquadric])
+    scene.show()
 
 def cos_pow(x, p):
-    return np.sign(np.cos(x)) * np.abs(np.cos(x))**p
+    return np.sign(np.cos(x)) * np.pow(np.abs(np.cos(x)), p)
 
 def sin_pow(x, p):
-    return np.sign(np.sin(x)) * np.abs(np.sin(x))**p
+    return np.sign(np.sin(x)) * np.pow(np.abs(np.sin(x)), p)
 
-for n in np.arange(-np.pi/2, np.pi/2, 0.1):
-    for w in np.arange(-np.pi, np.pi, 0.2):
-        x = a[0]*cos_pow(n,e1)*cos_pow(w,e2)
-        y = a[1]*cos_pow(n,e1)*sin_pow(w,e2)
-        z = a[2]*sin_pow(n,e1)
-        point = [x, y, z]
-        points.append(point)
-
-np_points = np.array(points)
-superquadric = trimesh.points.PointCloud(
-    vertices=np_points, colors=[0, 0, 255, 255]
-)
-
-scene = trimesh.Scene([superquadric])
-scene.show()
+if __name__ == '__main__': main()
